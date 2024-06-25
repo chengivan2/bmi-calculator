1015f84178abe9f0fab04fa4d71dd69b95a0c2bb
chengivan <chengoleivan@gmail.com>
2024-06-25 07:02:22 +0300

    Initial commit with title only

---
+++ src/app/components/header/Title.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../../componentsstsyles/header/Title.css'
+
+export const Title = () => {
+  return (
+    <div>
+      <h1 className="bmi-calc-title">BMI CALCULATOR</h1>
+    </div>
+  );
+};
